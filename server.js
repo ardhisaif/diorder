54dbf39bc02f8718e16bd4ab4c47688356dcac8a
ardhisaif <55237676+ardhisaif@users.noreply.github.com>
2021-05-21 15:34:40 +0700

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const app = express()
 const routes = require("./routes/index")
+const port = process.env.PORT || 4000
 
 app.set("view engine", "ejs")
 app.get('/', (req, res) => {
@@ -11,5 +12,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(routes)
 app.use( express.static( "public" ) );
 
-app.listen(3003)
+app.listen(port, function () {
+    console.log(`server start on ${port}`);
+})
 console.log('listening on port 3003');
